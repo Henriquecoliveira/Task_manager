0dc6300e00fc690db5403d96d3f62579e5e8c992
Henriquecoliveira <Henriquecordeiro395@outlook.com>
2026-07-18 08:56:51 -0300

    database config and server.js modified for cookie-paser

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 
+//middlewares
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
+
+//routes
+const userRoute = require("./src/router/user.js");
+app.use("/api", userRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running");
