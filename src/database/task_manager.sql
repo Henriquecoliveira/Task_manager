5407bac92c73b090cf587d05598360a655701b03
Henriquecoliveira <Henriquecordeiro395@outlook.com>
2026-07-21 18:25:04 -0300

    Group's name added in the database

--- src/database/task_manager.sql
+++ src/database/task_manager.sql
@@ -3,7 +3,7 @@ CREATE DATABASE Task_manager;
 USE Task_manager;
 
 CREATE TABLE users (
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     name VARCHAR(50) NOT NULL,
     role VARCHAR(10) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -13,6 +13,7 @@ CREATE TABLE users (
     
 CREATE TABLE `groups` (
 		id INT AUTO_INCREMENT NOT NULL,
+        name VARCHAR(50),
         PRIMARY KEY(id)  
         );
 
@@ -30,30 +31,45 @@ CREATE TABLE groups_members (
             REFERENCES users(email)
             ON DELETE CASCADE
 		);
-    
+        
+        SELECT `groups`.id, `groups`.name, `groups_members`.role_in_the_group
+        FROM `groups` 
+            INNER JOIN `groups_members`
+            ON `groups`.id = `groups_members`.group_id
+        WHERE `groups_members`.member = "rafaela@email.com";
+
 CREATE TABLE tasks (
 	id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(255),
-    `group` INT,
+    `group_id` INT NOT NULL,
     register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    author VARCHAR(100) NOT NULL,
+    author_email VARCHAR(100) NOT NULL,
+    status VARCHAR(10) NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT fk_tasks_groups
-		FOREIGN KEY (`group`)
+		FOREIGN KEY (`group_id`)
         REFERENCES `groups`(id)
+        ON DELETE CASCADE,
+	CONSTRAINT fk_tasks_users
+		FOREIGN KEY (author_email)
+        REFERENCES users (email)
         ON DELETE CASCADE
     );
-    
+        
 CREATE TABLE tasks_steps (
 	task_id INT NOT NULL,
     step INT NOT NULL,
     description VARCHAR(255) NOT NULL,
     status VARCHAR(10) NOT NULL,
-    author VARCHAR(100) NOT NULL,
+    author_email VARCHAR(100) NOT NULL,
     PRIMARY KEY(task_id, step),
     CONSTRAINT fk_tasks_steps_tasks
 		FOREIGN KEY (task_id)
         REFERENCES tasks(id)
+        ON DELETE CASCADE,
+	CONSTRAINT fk_tasks_steps_users
+		FOREIGN KEY (author_email)
+        REFERENCES users (email)
         ON DELETE CASCADE
-        );
+	);
